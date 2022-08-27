cab1b68751569e66b8d51fcbfa25821e947e5898
Fabián Villena <villenafabian@gmail.com>
2022-08-26 20:54:58 -0400

    view more random medications

--- conreceta/medications/views.py
+++ conreceta/medications/views.py
@@ -11,7 +11,9 @@ def index(request):
     return render(request, 'medications/index.html', {'medications': random_items})
 def detail(request, concept_id):
     try:
+        medications = list(Medication.objects.all())
+        random_medications = random.sample(medications, 5)
         medication = Medication.objects.get(pk=concept_id)
     except Medication.DoesNotExist:
         raise Http404("Medication does not exist")
-    return render(request, 'medications/detail.html', {'medication': medication})
+    return render(request, 'medications/detail.html', {'medication': medication, 'random_medications':random_medications})
