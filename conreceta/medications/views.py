3d1f4cb6c1a8e0daaf096fedb57918a780a4c785
Fabián Villena <villenafabian@gmail.com>
2022-09-03 17:04:25 -0400

    siblings

--- conreceta/medications/views.py
+++ conreceta/medications/views.py
@@ -15,9 +15,10 @@ def detail(request, concept_id):
         medications = list(CommercialProduct.objects.all())
         random_medications = random.sample(medications, 5)
         medication = CommercialProduct.objects.get(pk=concept_id)
+        siblings = medication.clinical_medication.commercialproduct_set.all()
     except CommercialProduct.DoesNotExist:
         raise Http404("Medication does not exist")
-    return render(request, 'medications/detail.html', {'medication': medication, 'random_medications':random_medications})
+    return render(request, 'medications/detail.html', {'medication': medication, 'random_medications':random_medications, 'siblings':siblings})
 def search_results(request):
     q = request.GET.get('q', '')
     results = CommercialProduct.objects.filter(
